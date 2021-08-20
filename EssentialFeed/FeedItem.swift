81b91b11cccfd73b025ec128524bb6cbc5804f20
Lucas Brito <lucasbrito_9@hotmail.com>
2021-08-20 08:36:30 -0300

    Create FeedItem model.

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Lucas Santana Brito on 20/08/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
